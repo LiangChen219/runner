9b902a1817312cc6d296f197b30fd0f2c723f96c
LiangChen219 <75097550+LiangChen219@users.noreply.github.com>
2023-10-08 19:52:22 +0100

    Add files via upload

--- runner.py
+++ runner.py
@@ -1,32 +1,91 @@
 import pygame
 import sys
-#initialises pygame
+
+GROUNDHEIGHT = 300
+#initialises pygame and creates display surface
 pygame.init()
-snailXpos = 800
-#creates display surface
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
+
 clock = pygame.time.Clock()
+
+#loading images and texts
+sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
+
 text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/Ground.png')
-text_surface = text_font.render('My Game', False, 'Red')
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-player_surface = pygame.image.load('graphics/player/jump.png')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (800, GROUNDHEIGHT))
+
+player_surface = pygame.image.load('graphics/player/jump.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, GROUNDHEIGHT))
+
+def display_score(time_before):
+    current_time = int(pygame.time.get_ticks()/100)
+    score_surface = text_font.render(f'{current_time-time_before}', True, 'Red')
+    score_rect = score_surface.get_rect(center = (400, 50))
+    screen.blit(score_surface, score_rect)
+player_acceleration = 0
+time_before = 0
+gameOver = False
 while True:
-    snailXpos -= 4 
+    #displays sky, ground, snail and player
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0, GROUNDHEIGHT))
+    display_score(time_before)
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
-    #places a regular surface over a display surface
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 50))
-    screen.blit(snail_surface, (snailXpos, 270))
-    screen.blit(player_surface, (100, 230))
-    pygame.display.update()
-    
+        if gameOver == False:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    #double jump
+                    if jumpTimes <= 1:
+                        #jump
+                        player_acceleration = -15
+                        jumpTimes += 1
+        #game is over
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                    gameOver = False 
+                
+    if gameOver == False:   
+        #moving snail
+        screen.blit(snail_surface, snail_rect)
+        snail_rect.x -= 4
+        if snail_rect.right < 0: snail_rect.left = 800
+
+        #moving player for jump
+        player_acceleration += 1
+        player_rect.y += player_acceleration
+
+        #if player on or below ground
+        if player_rect.bottom >= 300:
+            jumpTimes = 0
+            player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
+
+        #collision detection
+        if snail_rect.colliderect(player_rect):
+            gameOver = True
+    else:
+        screen.fill('Yellow')
+        replay = text_font.render('Return button to play again', True, 'Red')
+        replay_rect = replay.get_rect(center = (400, 50))
+        screen.blit(replay, replay_rect)
+        time_before = int(pygame.time.get_ticks()/100)
+        snail_rect.left = 800
+        
+
     clock.tick(60)
+
+    pygame.display.update()
+
+
+
+
+    
